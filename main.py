6fd228750d3da75ae93c86398ec4d2d4212eb31d
Shubham Pandey <s22333452@gmail.com>
2026-02-08 16:40:54 +0530

    Initial setup commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import os
+import sys   
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+    
+    #if len(sys.argv) < 2:
+    #    print("Prompt is empty. Please provide a prompt as a command-line argument.")
+    #    sys.exit(1)
+    #    return
+    #prompt = sys.argv[1]
+    
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt`
+    # We can use messages to structure the conversation, but for a simple prompt, we can directly pass it to the generate_content method.
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', contents=messages
+    )
+
+    # Always print the AI's answer first -->    
+     
+    print(response.text)   
+    if response is None or response.usage_metadata is None:
+        print("No usage metadata available.")
+        return
+
+    # Only print metadata if the flag was used
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        if response.usage_metadata:
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+            print(f"Total tokens: {response.usage_metadata.total_token_count}") 
+    
+
+        
+main()
