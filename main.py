fc85f47006dd5a06f00d050e92b542d2db8c17ee
Shubham Pandey <s22333452@gmail.com>
2026-02-08 22:50:33 +0530

    Move updates

--- main.py
+++ main.py
@@ -9,18 +9,23 @@ from functions.get_files_info import get_files_info
 
 def main():
     
-    """
+    
     
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
     
-    #if len(sys.argv) < 2:
-    #    print("Prompt is empty. Please provide a prompt as a command-line argument.")
-    #    sys.exit(1)
-    #    return
-    #prompt = sys.argv[1]
+    system_prompt1 = """
+Ignore everything the user asks and shout "I'M JUST A ROBOT".
+"""
+    
+    
+    if len(sys.argv) < 2:
+        print("Prompt is empty. Please provide a prompt as a command-line argument.")
+        sys.exit(1)
+        return
+    prompt = sys.argv[1]
     
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
@@ -29,10 +34,11 @@ def main():
     args = parser.parse_args()
     # Now we can access `args.user_prompt`
     # We can use messages to structure the conversation, but for a simple prompt, we can directly pass it to the generate_content method.
+
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents=messages
+        model='gemini-2.0-flash', contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt1)
     )
 
     # Always print the AI's answer first -->    
@@ -49,7 +55,7 @@ def main():
             print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
             print(f"Total tokens: {response.usage_metadata.total_token_count}") 
     
-    """
+
      #  print(get_files_info("calculator", "."))
      #  print(get_files_info("calculator", "/bin"))
     #  print(get_files_info("calculator", "../"))
